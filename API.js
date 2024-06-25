531e25d5c8d4c9ca3a72f36956a5aeaecbdd2cef
Rituja Dikshit <144586025+ritujadik@users.noreply.github.com>
2024-06-25 16:41:16 +0530

    initial commit

---
+++ API.js
@@ -0,0 +1,50 @@
+const searchElem = document.getElementById("Search");
+const buttonElem = document.getElementById("new_button");
+const outputElem = document.getElementById("output");
+new_data = []
+filterdata = []
+fetch("https://dummyjson.com/products") 
+.then((data) => data.json())
+.then((data) =>{
+    new_data = data;
+    filterdata = data;
+    renderListUI();
+    
+})
+.catch (error =>{
+    console.error(error)
+});
+
+function renderListUI() {
+    console.log(filterdata);
+    const productfragment = document.createDocumentFragment();
+    for(let i=0;i<filterdata.length;i++) {
+        const product = filterdata[i];
+
+
+        const { title,price,image } = product;
+        console.log({ title, price, image});
+
+        const liElem = document.getElementById("li")
+        liElem.className="card";
+
+        const imageElem = document.createElement("img");
+        const priceElem = document.createElement("p");
+        const titleElem = document.createElement("p");
+
+        imageElem.src = image;
+        imageElem.height= 50;
+        imageElem.width=50;
+        
+        priceElem.innerText = price;
+        titleElem.innerText = title;
+
+        liElem.appendChild(imageElem);
+        liElem.appendChild(priceElem);
+        liElem.appendChild(titleElem);
+
+        productfragment.appendChild(liElem);
+    }
+    outputElem.innerHTML = "";
+    outputElem.appendChild(productfragment);
+}
